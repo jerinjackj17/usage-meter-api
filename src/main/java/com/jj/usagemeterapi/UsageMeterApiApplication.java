37e35b6bafaed420383425d18e96bbf168874939
jerinjackj17 <jjerinjack@gmail.com>
2026-01-07 23:40:23 -0600

    he

--- src/main/java/com/jj/usagemeterapi/UsageMeterApiApplication.java
+++ src/main/java/com/jj/usagemeterapi/UsageMeterApiApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsageMeterApiApplication {
+public class
+
+
+
+UsageMeterApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsageMeterApiApplication.class, args);
